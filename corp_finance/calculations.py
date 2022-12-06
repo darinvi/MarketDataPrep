d6632d5324ef01116287d76047c62683ba18d5fe
darinvi <d.v.ivanov98@gmail.com>
2022-12-07 00:31:52 +0200

    calc and filter deleted as they used lists and functionality is gonna be different now

--- corp_finance/calculations.py
+++ corp_finance/calculations.py
@@ -1,35 +1,12 @@
 import statsmodels.formula.api as smf
 
 
-def simple_regression(df,dep,indep):
-    
+def simpleRegression(df,dep,indep):
     regg = smf.ols(formula=f"{dep} ~ {' + '.join(indep)}", data=df).fit()
     print(regg.summary())
 
-# def simple_regression(df,dep_var:str,indep_var:str):
-    
-#     dependent_variable = df[dep_var]
-#     independent_variable = df[indep_var]
-    
-#     sm.add_constant(independent_variable)
-#     est = sm.OLS(dependent_variable, independent_variable).fit()
-#     print(est.summary())
+def extremeCloseVixCounter(df):
+    pass
 
-
-def extreme_close_vix_counter(df):
-    df = df.values.tolist()
-    df_as_list = [int(df_as_list[1]) for df_as_list in df if df_as_list[9]>0]
-    print(df_as_list.count(1),df_as_list.count(2),df_as_list.count(3),df_as_list.count(4),df_as_list.count(5),df_as_list.count(6))
-    
-
-def close_held_gap(df_filltered):
-    df_filltered = df_filltered.values.tolist()
-    gap_held_counter = 0
-    for i in range(len(df_filltered)):
-        gap = df_filltered[i][8]
-        close_extreme = df_filltered[i][9]
-        gap_held = bool((gap < 0 and close_extreme == 2) or (gap > 0 and close_extreme == 1))
-        print(gap,close_extreme,gap_held)
-        if gap_held:
-            gap_held_counter += 1
-    print(gap_held_counter,len(df_filltered),f"{gap_held_counter/len(df_filltered)*100:.2f}%")
+def closeHeldGap(df_filltered):
+    pass
--- corp_finance/filtered_data.py
+++ corp_finance/filtered_data.py
@@ -2,15 +2,4 @@ import statistics
 import pandas as pd
 
 def gaps_outside_st(df):
-    standard_deviation = statistics.stdev(df["Chng"])
-    average_gap = statistics.mean(df["Chng"])
-    df = df.values.tolist()
-    df_filltered = []
-    for i in range(len(df)):
-        if df[i][8] > average_gap + standard_deviation or df[i][8] < average_gap - standard_deviation:
-            df_filltered.append(df[i])
-    df_filltered = pd.DataFrame(df_filltered)
-    df_filltered.columns = ["VIX","VIX_Coded","TLT","Open","High","Low","Close","Volume","Chng","Close_Extreme","Wrong","Rvol","ATR","Rrng"]
-    return df_filltered
-
-
+    pass
